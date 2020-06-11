32182bc8b10fe9c90fde6ac0302e246c3f59cb5b
PDC <uottawa.pdc@gmail.com>
2020-06-11 12:19:16 -0400

    f

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const PORT= process.env.PORT || 8080;  //Step 1
 const routes = require('./routes/api');
 
 //connect with mongo
-//const MONGOdb_URI = 'mongodb+srv://yiyinzhang:602390489stop@youtubedb.1pt6s.mongodb.net/<dbname>?retryWrites=true&w=majority';
-
+//const MONGOdb_URI = 'mongodb+srv://yiyinzhang:602390489stop@youtubedb.1pt6s.mongodb.net/YoutubeDB?retryWrites=true&w=majority';
+//mongodb+srv://yiyinzhang:602390489stop@youtubedb.1pt6s.mongodb.net/YoutubeDB?retryWrites=true&w=majority
 //Step 2
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/mern_youtube', {   
     useNewUrlParser: true,
