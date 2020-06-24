6cf15a4eabb68ed61032115478a66b4937394447
mangosata <mangosata@gmail.com>
2020-06-24 18:59:50 -0400

    added create projece page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React from "react";
 import Router from "./Router";
+import "semantic-ui-css/semantic.min.css";
 import "./App.less";
-
+import { Menu, Container, Button } from "semantic-ui-react";
 
 const App = () => {
-    return (
-        <div>
-            <Router/>
-        </div>
-    )
+  return (
+    <div>
+      {/* Header */}
+      <Menu fixed="top" inverted>
+        <Container>
+          <Menu.Item as="a" header>
+            Professional Development Club
+          </Menu.Item>
+          <Menu.Item as="a">Home</Menu.Item>
+          <Menu.Item position="right">
+            <Button as="a" inverted>
+              Log in
+            </Button>
+            <Button as="a" inverted style={{ marginLeft: "0.5em" }}>
+              Sign Up
+            </Button>
+          </Menu.Item>
+        </Container>
+      </Menu>
+
+      <Container text style={{ marginTop: "7em" }}>
+        <Router />
+      </Container>
+    </div>
+  );
 };
 
 export default App;
--- client/src/view/CreateProject/index.js
+++ client/src/view/CreateProject/index.js
@@ -1,35 +1,66 @@
-import React, {useState} from 'react';
-import {useForm} from "react-hook-form";
-import axios from 'axios';
-
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { Button, Form, Input, TextArea } from "semantic-ui-react";
 
 const CreateProject = () => {
+  const [info, setInfo] = useState({
+    name: "",
+    email: "",
+    project: "",
+  });
 
-        const classes = useStyles();
+  const handleChange = event => {
+    setInfo({
+      ...info,
+      [event.target.name]: event.target.value,
+    });
+  }
 
-        const {register, handleSubmit} = useForm();
+  const handleSubmit = (event) => {
+    console.log(info);
+    
+    axios({
+      url: "http://localhost:8080/api/save",
+      method: "POST",
+      data: info,
+    })
+      .then(() => {
+        console.log("data has  been send to the server");
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log("data has been not sent the server");
+      });
+  };
 
-        const onSubmit = (data) => {
-            console.log("submitting!");
-            axios({
-                url: 'http://localhost:8080/api/save',
-                method: 'POST',
-                data: data
-            })
-                .then(() => {
-                    console.log('data has  been send to the server');
-                })
-                .catch((error) => {
-                    console.log(error);
-                    console.log('data has been not sent the server')
-                })
-        };
-
-
-        return (
-
-        )
-    }
-;
-
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group widths="equal">
+        <Form.Input
+          label="Name"
+          placeholder="Name"
+          name="name"
+          value={info.name}
+          onChange={handleChange}
+        />
+        <Form.Input
+          label="Email"
+          placeholder="Email"
+          value={info.email}
+          name="email"
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Input
+        label="Project"
+        placeholder="Project Information"
+        value={info.project}
+        name="project"
+        onChange={handleChange}
+      />
+      <Form.Field control={Button}>Submit</Form.Field>
+    </Form>
+  );
+};
 export default CreateProject;
