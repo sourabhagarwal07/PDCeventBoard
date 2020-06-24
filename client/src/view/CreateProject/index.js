e49583c5c6f21cc5ca3cd1eb3ab9acde26f5b659
mangosata <mangosata@gmail.com>
2020-06-24 17:52:47 -0400

    added create project test page

--- client/src/view/CreateProject/index.js
+++ client/src/view/CreateProject/index.js
@@ -1,34 +1,9 @@
 import React, {useState} from 'react';
-import Container from '@material-ui/core/Container';
-import CssBaseline from "@material-ui/core/CssBaseline";
-import makeStyles from "@material-ui/core/styles/makeStyles";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
 import {useForm} from "react-hook-form";
 import axios from 'axios';
 
 
 const CreateProject = () => {
-
-        const useStyles = makeStyles((theme) => ({
-            paper: {
-                marginTop: theme.spacing(8),
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-            },
-            avatar: {
-                margin: theme.spacing(1),
-                backgroundColor: theme.palette.secondary.main,
-            },
-            form: {
-                width: '100%', // Fix IE 11 issue.
-                marginTop: theme.spacing(1),
-            },
-            submit: {
-                margin: theme.spacing(3, 0, 2),
-            },
-        }));
 
         const classes = useStyles();
 
@@ -52,57 +27,6 @@ const CreateProject = () => {
 
 
         return (
-            <Container component="main" maxWidth="xs">
-                <CssBaseline/>
-                <div className={classes.paper}>
-                    <form className={classes.form} onSubmit={handleSubmit(onSubmit)} noValidate>
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="name"
-                            label="Name"
-                            name="name"
-                            autoFocus
-                            inputRef={register}
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                            inputRef={register}
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="project"
-                            label="Project"
-                            name="project"
-                            autoFocus
-                            inputRef={register}
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                        >
-                            Submit
-                        </Button>
-                    </form>
-                </div>
-
-            </Container>
 
         )
     }
