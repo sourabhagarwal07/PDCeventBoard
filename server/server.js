7e06b9281f262457dc7e4adfc3621bd4591dad1f
binjiasata <bjia038@uottawa.ca>
2020-07-03 01:27:14 -0400

    deploy on heroku

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080; //Step 1
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use('/', express.static(path.join(__dirname, "build")));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
