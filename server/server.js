282141da82a5f98b509a8c0fbb748e65abd0ed2c
binjiasata <bjia038@uottawa.ca>
2020-07-02 22:24:44 -0400

    build and deploy

--- server/server.js
+++ server/server.js
@@ -14,10 +14,10 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080; //Step 1
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.all("*", function (req, res, next) {
