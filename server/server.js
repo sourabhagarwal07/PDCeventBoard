dd93a5c2ebef78206ec69df49657e5688cfff3cf
binjiasata <bjia038@uottawa.ca>
2020-07-05 12:28:21 -0400

    deleted some comments

--- server/server.js
+++ server/server.js
@@ -14,11 +14,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080; //Step 1
 
-app.use('/', express.static(path.join(__dirname, "build")));
-
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+app.use("/", express.static(path.join(__dirname, "build")));
 
 app.all("*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
