8efd83475a646c89a1b57cdfb62e44469679a17f
binjiasata <bjia038@uottawa.ca>
2020-07-07 15:48:10 -0400

    fixed bugs

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-const express = require('express');
+const express = require("express");
 const database = require("./shared/config/Database");
-const morgan = require('morgan');
-const cors = require('cors');
+const morgan = require("morgan");
+const cors = require("cors");
 const cookieSession = require("cookie-session");
 const passportSetup = require("./shared/config/Passport");
 const passport = require("passport");
 const keys = require("./shared/config/Keys");
 const authRoutes = require("./shared/routes/Auth");
-const mainRoutes = require("./shared/routes/MainRoute")
-const http = require('http');
-const path = require('path');
-
+const mainRoutes = require("./shared/routes/MainRoute");
+const http = require("http");
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 8080;  //Step 1
+const PORT = process.env.PORT || 8080; //Step 1
 
-if (process.env.NODE_ENV === 'production') {
-  console.log("dir name",__dirname);
-  app.use('/',express.static(path.join(__dirname, '/client/build'))); 
+if (process.env.NODE_ENV === "production") {
+  console.log("dir name", __dirname);
+  app.use("/", express.static(path.join(__dirname, "/client/build")));
 }
 
-app.all('*', function (req, res, next) {
-  //res.header("Access-Control-Allow-Origin", keys.serverurl);
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
+  );
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By", ' 3.2.1');
+  res.header("X-Powered-By", " 3.2.1");
 
   if (req.method == "OPTIONS") {
     res.send(200);
-  }
-  else {
+  } else {
     next();
   }
 });
 
-//app.use(cors());
 // use cookie-session to encrypt user.id
 app.use(
   cookieSession({
@@ -50,12 +50,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // log output
-app.use(morgan('tiny')); 
-
-app.use(cors());
+app.use(morgan("tiny"));
 
 // auth router
-app.use('/auth', authRoutes);
-app.use('/', mainRoutes);
+app.use("/auth", authRoutes);
+app.use("/", mainRoutes);
 
 app.listen(PORT, () => console.log(`Server is starting at ${PORT}`));
