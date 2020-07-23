07324a11517761cd0a1f800d9ada1945ab4379fb
binjiasata <bjia038@uottawa.ca>
2020-07-23 19:13:17 -0400

    Fixed cannot GET bug

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const cookieSession = require("cookie-session");
 const passportSetup = require("./shared/config/Passport");
-const passportLinkedin = require("./shared/config/PassportLinkedin")
+const passportLinkedin = require("./shared/config/PassportLinkedin");
 const passport = require("passport");
 const keys = require("./shared/config/Keys");
 const authRoutes = require("./shared/routes/Auth");
@@ -61,19 +61,22 @@ app.use(
 // initialize passport
 app.use(passportLinkedin.initialize());
 
-
-app.get('/auth/linkedin',
-  passportLinkedin.authenticate('linkedin', { state: 'SOME STATE'  }),
-  function(req, res){
+app.get(
+  "/auth/linkedin",
+  passportLinkedin.authenticate("linkedin", { state: "SOME STATE" }),
+  function (req, res) {
     // The request will be redirected to LinkedIn for authentication, so this
     // function will not be called.
-});
+  }
+);
 
-app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
-    successRedirect: '/',
-    failureRedirect: '/login'
-}));
-
+app.get(
+  "/auth/linkedin/callback",
+  passport.authenticate("linkedin", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
 
 app.use(passportLinkedin.session());
 
@@ -83,5 +86,11 @@ app.use(morgan("tiny"));
 // auth router
 app.use("/auth", authRoutes);
 app.use("/", projectRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "the path to your react project", "index.html")
+  );
+});
 
 app.listen(PORT, () => console.log(`Server is starting at ${PORT}`));
