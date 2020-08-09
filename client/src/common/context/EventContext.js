a15b30a4dea670b46230380228482eebfa36ee51
Yiyin Zhang <yzhan647@uottawa.ca>
2020-08-09 17:23:46 -0400

    delete useless code in event page

--- client/src/common/context/EventContext.js
+++ client/src/common/context/EventContext.js
@@ -7,21 +7,8 @@ export const EventsConsumer = EventsContext.Consumer;
 export {EventsContext};
 
 const EventsProvider = (props) => {
-  // const url ="https://www.eventbriteapi.com/v3/organizations/464741062423/events/?token=2SWITQPH72SPNCSRK7OW";
 
   const [eventInfo, setEventInfo] = useState([]);
-
-  // axios.get(url)
-  //   .then((res) => {
-  //     return res.data;
-  //   })
-  //   .then((data) => {
-  //     //console.log(data);
-  //     setEventInfo(data);
-  //   })
-  //   .catch((e) => {
-  //     console.log(e);
-  //   });  
 
   return (
     <EventsContext.Provider
@@ -31,31 +18,5 @@ const EventsProvider = (props) => {
     </EventsContext.Provider>
   );
 }
-
-// export {EventsProvider, EventsContext};
-
-// class EventsProvider extends Component {
-//   state = { events: [] };
-
-//   getEvents = async () => {
-//     const url ="https://www.eventbriteapi.com/v3/organizations/464741062423/events/?token=2SWITQPH72SPNCSRK7OW";
-
-//     const response = await axios.get(url);
-//     const { events } = response.data;
-//     console.log(events);
-
-//     this.setState({ events });
-//   };
-
-//   render() {
-//     return (
-//       <EventsContext.Provider
-//         value={{ events: this.state.events, getEvents: this.getEvents }}
-//       >
-//         {this.props.children}
-//       </EventsContext.Provider>
-//     );
-//   }
-// }
 
 export default EventsProvider;
--- client/src/view/Events/Events.js
+++ client/src/view/Events/Events.js
@@ -10,29 +10,8 @@ import { config } from "../../common/config/config";
 const Events = (props) => {
 
   const { eventInfo, setEventInfo } = useContext(EventsContext);
-  // let events = [];
-  // console.log(typeof(events));
-  // //events = Object.keys(eventInfo.events)
-  // console.log(typeof(events));
-  // console.log(events);
-
-  // const [ eventInfo, setEventInfo ] = useState();
-  // const path = config();
 
   const url ="https://www.eventbriteapi.com/v3/organizations/464741062423/events/?token=2SWITQPH72SPNCSRK7OW";
-
-  // axios.get(url)
-  //   .then((res) => {
-  //     return res.data;
-  //   })
-  //   .then((data) => {
-  //     console.log(data);
-  //     setEventInfo(data);
-  //   })
-  //   .catch((e) => {
-  //     console.log(e);
-  //   });
-  //   console.log(eventInfo);
 
   useEffect(async () => {
     console.log("effective currently")
@@ -61,31 +40,4 @@ const Events = (props) => {
   );
 }
 
-// export default Events;
-// const onClick = (value) => {
-//   value.getEvents();
-// };
-
-// const Events = () => {
-//   return (
-//     <div className="uk-child-width-1-3@m" uk-grid="true">
-//       <Segment>
-//         <EventsConsumer>
-//           {(value) => {
-//             return <Button onClick={value.getEvents}>Show events</Button>;
-//           }}
-//         </EventsConsumer>
-//       </Segment>
-//         <Card.Group itemsPerRow={3}>
-//         <EventsConsumer>
-//           {(value) =>
-//             value.events.map((event) => {
-//               return <EventCard key={event.id} event={event} />;
-//             })
-//           }
-//         </EventsConsumer>
-//         </Card.Group>
-//     </div>
-//   );
-// };
 export default Events;
