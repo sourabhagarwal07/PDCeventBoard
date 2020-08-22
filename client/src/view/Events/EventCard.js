4eb3b96373e0608dcd29ab00adff957f6f89f449
Yiyin Zhang <yzhan647@uottawa.ca>
2020-08-21 21:53:29 -0400

    changed event time layout

--- client/src/view/Events/EventCard.js
+++ client/src/view/Events/EventCard.js
@@ -7,6 +7,24 @@ const EventCard = ({ event }) => {
     text = `${text.substr(0, 250)}...`;
   }
 
+  const timeDisplay = (time) => {
+    console.log(time);
+    //example: time = "2020-08-15T07:30:00"
+    time = time.split("T");  // time = ["2020-08-15", "07:30:00"]
+    let time0 = time[0].split("-"); // time0 = ["2020", "08", "15"];
+    let time1 = time[1].split(":"); // time1 = ["07", "30", "00"]
+    let timeD = time0[2]+"/"+time0[1]+"/"+time0[0];  //timeD="15/08/2020"
+    let ampm = "AM";
+    if(time1[0] >= 12 ){
+      ampm = "PM";
+      if(time1[0] > 12){
+        time1[0] = time1[0] -12;
+      }
+    }
+    let timeT = time1[0]+":"+time1[1]+" "+ampm;  //timeT="07:30 AM"
+    return time = timeD+" "+" "+timeT;
+  }
+
   return (
     <Fragment>
       <Card color='blue'>
@@ -14,10 +32,10 @@ const EventCard = ({ event }) => {
         <Card.Content>
         <Card.Header>{event.name.text}</Card.Header>
         <Card.Meta>
-          <span>Start time: {event.start.utc}</span>
+          <span>Start time: {timeDisplay(event.start.local)}</span>
         </Card.Meta>
         <Card.Meta>
-          <span>End time: {event.end.utc}</span>
+          <span>End time: {timeDisplay(event.end.local)}</span>
         </Card.Meta>
         <Card.Description>
           {event.description.text}
