ffa507bb034bb2be79683208d13f309856aa7376
abhiram040 <varma.abhiram97@gmail.com>
2020-08-28 12:46:54 -0400

    static changes in our team page and creation of useful resources links for new students

--- client/src/view/Student/GNG5299.js
+++ client/src/view/Student/GNG5299.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import {Button} from "semantic-ui-react";
+import {Button,Grid,Segment,Image} from "semantic-ui-react";
 
 
 const GNG5299 = (props) => {
@@ -9,30 +9,61 @@ const GNG5299 = (props) => {
   };
   return (
     <Fragment>
-      <h1 class="ui center aligned huge header">Project proposal Template</h1>
-
-      <p>
-        In order to register for GNG5299, students must complete a Project
-        Proposal Form (fill in the attached template below) that must be
-        approved and signed by one of the two Project Coordinators:{" "}
-      </p>
-      <p>
-        Mana Azarm (mazar088@uottawa.ca), Office hours: Monday, 1-3pm in SITE
-        3020
-      </p>
-      <p>
-        Sawsan Abdul-Majid (sawsan.majid@uottawa.ca) Office hours: Friday, 12 –
-        1:30pm in SITE 2051a.
-      </p>
-      <p>
-        Students thinking of doing a GNG 5299 Project should attend the
-        information sessions every Thursday from 4pm to 530pm in CRX-010.
-      </p>
-      <p>
-        Once the proposal is approved you submit the signed project proposal
-        form as a service request to your program.
-      </p>
-      <Button primary onClick={handledownload}>Download</Button>
+      <Segment placeholder>
+        <Grid columns={2} stackable textAlign="center">
+          <Grid.Row verticalAlign="middle">
+            <Grid.Column>
+              <h1>Useful University Resources for Newly Admitted Students</h1>
+            </Grid.Column>
+            <Grid.Column>
+              <Image centered size="large" src="/assets/students-uottawa.jpg" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <div class="ui inverted segment">
+        <div class="ui inverted relaxed divided list">
+          <a class="item" href="https://international.uottawa.ca/en">
+          International Office 
+          </a>
+          <a class="item" href="https://international.uottawa.ca/en/study-at-uottawa/current-students/immigration#1">
+          Immigration
+          </a>
+          <a class="item" href="https://international.uottawa.ca/en/study-at-uottawa/uoconnexion">
+          uOConnexion 
+          </a>
+          <a class="item" href="https://international.uottawa.ca/en/study-at-uottawa/uhip">
+          University Health Insurance Plan (UHIP)
+          </a>
+          <a
+            class="item"
+            href="https://www.uottawa.ca/graduate-studies/students/awards"
+          >
+            Awards and Financial Support
+          </a>
+          <a
+            class="item"
+            href="https://www.uottawa.ca/wellness/"
+          >
+            Mental Health and Wellness 
+          </a>
+          <a class="item" href="https://www.uottawa.ca/career-development-centre/">
+          Career Development Centre 
+          </a>
+          <a class="item" href="https://sass.uottawa.ca/en/writing">
+          Academic Writing Help Centre (AWHC)
+          </a>
+          <a class="item" href="https://olbi.uottawa.ca/">
+          Official Language and Bilingualism Institute (OLBI)
+          </a>
+          <a class="item" href="https://gsaed.ca/en/home/">
+          Graduate Students Association of the University of Ottawa (GSAED) 
+          </a>
+          <a class="item" href="https://www.uottawa.ca/important-academic-dates-and-deadlines/">
+          Important Academic Dates and Deadlines 
+          </a>
+        </div>
+      </div>
     </Fragment>
   );
 };
