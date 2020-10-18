db02a2cab85dfa9d00ebb6a8b4f9a7c3b7430df8
navpreet kaur <65980427+navpreetkaur051@users.noreply.github.com>
2020-10-18 18:26:49 -0400

    feedback form change

--- client/src/view/Feedback/Feedback.js
+++ client/src/view/Feedback/Feedback.js
@@ -8,7 +8,9 @@ const Feedback = (props) => {
                 <GridRow>
                 <Grid.Column class="text">
                 <h3 class="ui center aligned header">Please share your website experience with us. Your feedback help us improve.</h3>
-                <Button variant="outline-dark" href="https://docs.google.com/forms/d/e/1FAIpQLSdPQgcmGqcqlMpWfqdvuE2FWxXMeURalkNPQOku9txNmLmhYw/viewform?embedded=true" 
+                {/* <Button variant="outline-dark" href="https://docs.google.com/forms/d/e/1FAIpQLSdPQgcmGqcqlMpWfqdvuE2FWxXMeURalkNPQOku9txNmLmhYw/viewform?embedded=true" 
+                size='lg' className="survey-reminder" target="_blank"> */}
+                <Button variant="outline-dark" href="https://forms.office.com/Pages/ResponsePage.aspx?id=sdof1BV-_Uy1-nIA5U3ra8TWvX0Di8dIjAEMQgqNdlpUQ1kwRlRWQ1lVSjc1VDFFRkJQTUVZQllaRy4u" 
                 size='lg' className="survey-reminder" target="_blank">
                 <h4>
                    Take the Survey 
